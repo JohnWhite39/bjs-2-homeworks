81ce9972b544530031d494f7ebd4d77944678a25
JohnWhite39 <124592559+JohnWhite39@users.noreply.github.com>
2024-04-03 16:46:31 +0200

    Update task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -22,3 +22,11 @@ function calculateTotalMortgage(percent, contribution, amount, countMonths) {
   
   return parseFloat(totalMortgage.toFixed(2));
 }
+
+console.log(calculateTotalMortgage(10, 0, 50000, 12)); 
+console.log(calculateTotalMortgage(10, 1000, 50000, 12)); 
+console.log(calculateTotalMortgage(10, 0, 20000, 24)); 
+console.log(calculateTotalMortgage(10, 1000, 20000, 24)); 
+console.log(calculateTotalMortgage(10, 20000, 20000, 24)); 
+console.log(calculateTotalMortgage(10, 0, 10000, 36)); 
+console.log(calculateTotalMortgage(15, 0, 10000, 36)); 
