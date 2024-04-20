38ac8363fc6aec1617b5d3fe323584da425b2f21
JohnWhite39 <124592559+JohnWhite39@users.noreply.github.com>
2024-04-20 14:58:05 +0200

    Update task.js

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -21,54 +21,56 @@ function getArrayParams(...arr) {
 console.log(getArrayParams(-99, 99, 10)); // { min: -99, max: 99, avg: 3.33 }
 
 function summElementsWorker(...arr) {
-  if (arr.length === 0) return 0;
-  return arr.reduce((acc, val) => acc + val, 0);
+  let sum = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+
+  return sum;
 }
-cosole.log(summElementsWorker());
 
 function differenceMaxMinWorker(...arr) {
-  if (arr.length === 0) return 0;
-  const max = Math.max(...arr);
-  const min = Math.min(...arr);
+  let min = Math.min(...arr);
+  let max = Math.max(...arr);
+
   return max - min;
 }
-console.log(differenceMaxMinWorker());
 
 function differenceEvenOddWorker(...arr) {
-  if (arr.length === 0) return 0;
-  let sumEvenElement = 0;
-  let sumOddElement = 0;
+  let sumEven = 0;
+  let sumOdd = 0;
 
-  for (i = 0; i < arr.length; i++) {
-    if(arr[i] % 2 === 0) {
-      sumEvenElement += arr[i];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      sumEven += arr[i];
     } else {
-      sumOddElement += arr[i];
+      sumOdd += arr[i];
     }
   }
-  return sumEvenElement - sumOddElement;
+
+  return sumEven - sumOdd;
 }
-console.log(differenceEvenOddWorker(94, 51, 57, 41, 47, 66, 58, 10, 38, 17));
 
 function averageEvenElementsWorker(...arr) {
-  if(arr.length === 0) return 0;
-  let sumEvenElement = 0;
-  let countEvenElement = 0;
+  let sumEven = 0;
+  let countEven = 0;
 
-  for (i = 0; i < arr.length; i++) {
+  for (let i = 0; i < arr.length; i++) {
     if (arr[i] % 2 === 0) {
-      sumEvenElement += arr[i];
-      countEvenElement++;
+      sumEven += arr[i];
+      countEven++;
     }
   }
-  return sumEvenElement / countEvenElement;
-}
-console.log(averageEvenElementsWorker(1, 2, 3, 4, 5, 6, 7, 8, 9));
 
-function makeWork (arrOfArr, func) {
+  return (sumEven / countEven).toFixed(2);
+}
+
+// Функция мясорубки
+function makeWork(arrOfArr, func) {
   let maxWorkerResult = -Infinity;
 
-  for (i = 0; < arrOfArr.length; i++) {
+  for (let i = 0; i < arrOfArr.length; i++) {
     const currentResult = func(...arrOfArr[i]);
 
     if (currentResult > maxWorkerResult) {
@@ -76,7 +78,7 @@ function makeWork (arrOfArr, func) {
     }
   }
 
-return maxWorkerResult; 
+  return maxWorkerResult;
 }
 
 const arr = [
@@ -85,4 +87,5 @@ const arr = [
   [72, 75, 51, 87, 43],
   [30, 41, 55, 96, 62],
 ];
+
 console.log(makeWork(arr, summElementsWorker));
