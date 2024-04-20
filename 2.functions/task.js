d4ebacb74d1401563a9d2c77e0b4d46c12307026
JohnWhite39 <124592559+JohnWhite39@users.noreply.github.com>
2024-04-20 13:53:48 +0200

    Update task.js

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -21,20 +21,49 @@ function getArrayParams(...arr) {
 console.log(getArrayParams(-99, 99, 10)); // { min: -99, max: 99, avg: 3.33 }
 
 function summElementsWorker(...arr) {
-
+  if (arr.length === 0) return 0;
+  return arr.reduce((acc, val) => acc + val, 0);
 }
+cosole.log(summElementsWorker());
 
 function differenceMaxMinWorker(...arr) {
-
+  if (arr.length === 0) return 0;
+  const max = Math.max(...arr);
+  const min = Math.min(...arr);
+  return max - min;
 }
+console.log(differenceMaxMinWorker());
 
 function differenceEvenOddWorker(...arr) {
+  if (arr.length === 0) return 0;
+  let sumEvenElement = 0;
+  let sumOddElement = 0;
 
+  for (i = 0; i < arr.length; i++) {
+    if(arr[i] % 2 === 0) {
+      sumEvenElement += arr[i];
+    } else {
+      sumOddElement += arr[i];
+    }
+  }
+  return sumEvenElement - sumOddElement;
 }
+console.log(differenceEvenOddWorker(94, 51, 57, 41, 47, 66, 58, 10, 38, 17));
 
 function averageEvenElementsWorker(...arr) {
+  if(arr.length === 0) return 0;
+  let sumEvenElement = 0;
+  let countEvenElement = 0;
 
+  for (i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      sumEvenElement += arr[i];
+      countEvenElement++;
+    }
+  }
+  return sumEvenElement / countEvenElement;
 }
+console.log(averageEvenElementsWorker(1, 2, 3, 4, 5, 6, 7, 8, 9));
 
 function makeWork (arrOfArr, func) {
 
