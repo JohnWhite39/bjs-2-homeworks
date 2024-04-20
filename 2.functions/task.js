054f5160c04cc98f1e35f17ee3ecc506930b1efa
JohnWhite39 <124592559+JohnWhite39@users.noreply.github.com>
2024-04-20 12:28:34 +0200

    Update task.js

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,7 +1,23 @@
 function getArrayParams(...arr) {
+  let min = Infinity;
+  let max = Infinity;
+  let sum = 0;
+  
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+    sum += arr[i];
+  }
 
+  const avg = Number((sum / arr.length).toFixed(2));
+  
   return { min: min, max: max, avg: avg };
 }
+console.log(getArrayParams(-99, 99, 10)); 
 
 function summElementsWorker(...arr) {
 
