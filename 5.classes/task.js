053a3e6497898107eb829b02f4dc7300a92929cb
JohnWhite39 <124592559+JohnWhite39@users.noreply.github.com>
2024-05-22 13:13:59 +0200

    Update task.js

---
+++ 5.classes/task.js
@@ -0,0 +1,71 @@
+class PrintEditionItem {
+  constructor(name, releaseDate, pagesCount) {
+    this.name = name;
+    this.releaseDate = releaseDate;
+    this.pagesCount = pagesCount;
+    this.state = 100;
+    this.type = null;
+  }
+
+  fix() {
+    if(this.state < 100) {
+      this.state *= 1.5;
+    }
+  }
+
+  set state(newState) {
+    if (newState < 0) {
+      this.state = 0;
+    } else if (newState > 100) {
+      this.state = 100;
+    } else {
+      this.state = newState;
+    }
+  }
+
+  get state () {
+    return this._state;
+  }
+}
+
+class Magazine extends PointeditionItem {
+  constructor(name, releaseDate, pagesCount) {
+    super(name, releaseDate, pagesCount);
+    this.type = "magazine";
+  }
+}
+
+class Book extends PointEditionItem {
+  constructor(author, name, releaseDate, pagesCount) {
+    super(name, releaseDate, pagesCount);
+    this.author = author;
+    this.type = "book";
+  }
+}
+
+class NovelBook extends Book {}
+class FantasticBook extends Book {}
+class DetectiveBook extends Book {}
+const sherlock = new PrintEditionItem(
+  "Полное собрание повестей и рассказов о Шерлоке Холмсе в одном томе",
+  2019,
+  1008
+);
+
+console.log(sherlock.releaseDate);
+console.log(sherlock.state);
+sherlock.fix();
+console.log(sherlock.state);
+
+const picknick = new FantasticBook(
+  "Аркадий и Борис Стругацкие",
+  "Пикник на обочине",
+  1972,
+  168
+);
+
+console.log(picknick.author);
+picknick.state = 10;
+console.log(picknick.state);
+picknick.fix();
+console.log(picknick.state);
